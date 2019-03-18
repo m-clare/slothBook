0ab34e6ebccf1fd569b3a18d71473e816a8cd701
Maryanne Wachter <wachter@arch.ethz.ch>
2019-03-17 22:28:51 -0400

    remove deprecated folder

--- deprecated/batch_execute.py
+++
@@ -1,9 +0,0 @@
-import papermill as pm
-import json
-
-index = 1
-fp = './beams_test.json'
-with open(fp) as f:
-	beam_data = json.load(f)
-for i in range(2):
-	pm.execute_notebook('./concrete_beam_design.ipynb', './nboutput/cb_out' + str(i) + '.ipynb', parameters=beam_data[i])
